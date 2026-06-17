1c05a42791384654bc25e348860693018b462e58
Freenstaip <111694697+Freenstaip@users.noreply.github.com>
2026-06-17 14:12:39 +0300

    Delete frontend directory

--- frontend/config.js
+++
@@ -1,4 +0,0 @@
-// Партнёрская ссылка для локального теста.
-// На Cloudflare Pages основная ссылка берётся из Environment Variable PARTNER_URL
-// и используется в /go?uid=... для учёта переходов.
-window.PARTNER_URL = 'https://partner-site.com';
